e50b48649f0dca9db1d40d06cef194c43cc591e2
stimadella-tech <stimadella@gmail.com>
2026-06-23 01:08:21 +0300

    Update data.js

--- data.js
+++ data.js
@@ -30,7 +30,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 2.0944
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
